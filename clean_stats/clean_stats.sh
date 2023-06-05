bb636d22b52c8c98043d0a562ffe0a45fd11ef7a
Georgi Uzunov <georgiuzunov87@abv.bg>
2023-06-05 17:37:43 +0300

    Update clean_stats.sh

--- clean_stats/clean_stats.sh
+++ clean_stats/clean_stats.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Set variable
+getdate=$(date +%Y-%m-%d)
+gettime=$(date +%H:%M:%S)
+sqlcleanstat=/root/URL-management/scripts/clean_stats/SQL_clean_stat
 
+mysql --table < $sqlcleanstat
+
+echo "Done at: $getdate on $gettime"
